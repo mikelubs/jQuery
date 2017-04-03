58e26fdbf15da6c028874a1eec5ec77108dbb48b
nauphrage <nauphragecompilers@gmail.com>
2017-04-03 20:56:51 +0300

    Added some css

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,11 @@
  $(document).ready(function() {
-        $("p").click(function() {
+        $(".clickable").click(function() {
           $("img").show();
         });
       });
 
  $(document).ready(function() {
-        $("p").click(function() {
+        $(".clickable").click(function() {
           $(".walrus-showing").toggle();
           $(".walrus-hidden").toggle();
         });
