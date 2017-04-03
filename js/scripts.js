14ac88fe1174dcb1758b750c05f800c51aaf2f89
nauphrage <nauphragecompilers@gmail.com>
2017-04-03 20:36:59 +0300

    toggle

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,6 @@
 
  $(document).ready(function() {
         $("p").click(function() {
-          $(".walrus-showing").show();
-          $(".walrus-hidden").hide();
-        });
-      });
-
- $(document).ready(function() {
-        $("p").click(function() {
           $(".walrus-showing").toggle();
           $(".walrus-hidden").toggle();
         });
